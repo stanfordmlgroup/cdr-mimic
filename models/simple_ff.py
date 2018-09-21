9139dfa52ae141573b31868585ee18d60e34d40d
Tony Duan <tony@tonyduan.com>
2018-09-21 11:36:26 -0700

    Replace ReLU with SELU.

--- models/simple_ff.py
+++ models/simple_ff.py
@@ -14,11 +14,11 @@ class SimpleNN(nn.Module):
 
         self.model = nn.Sequential(
             nn.Linear(self.num_demographics + self.embedding_dim, 128),
-            nn.ReLU(),
+            nn.SELU(),
             nn.Linear(128, 64),
-            nn.ReLU(),
+            nn.SELU(),
             nn.Linear(64, 64),
-            nn.ReLU(),
+            nn.SELU(),
             nn.Linear(64, 2),
         )
 
